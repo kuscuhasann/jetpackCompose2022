ef8265eb53cbee7cee7e82abc7382bde4dc04457
kuscuhasann <kuscuhasann97@gmail.com>
2022-08-19 12:09:04 +0300

    State yapısı ile çalışmak

--- app/src/main/java/com/pakt_games/jetpackcompose2022/MainActivity.kt
+++ app/src/main/java/com/pakt_games/jetpackcompose2022/MainActivity.kt
@@ -4,19 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.pakt_games.jetpackcompose2022.ui.theme.JetpackCompose2022Theme
+import org.intellij.lang.annotations.JdkConstants
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,9 +39,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainPage() {
-    Row {
-        Box(modifier = Modifier.size(80.dp).background(Color.Magenta))
-        Box(modifier = Modifier.size(80.dp).background(Color.Red))
+    var counter = remember {
+        mutableStateOf(0)
+    }
+    Column(
+        //horizontalAlignment = Yatayda Hizalama
+        horizontalAlignment = Alignment.End,
+        //verticalArrangement = Dikeyde Hizalama Yapar
+        verticalArrangement = Arrangement.Center,
+        //Sayfaya kendini yayar.
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text ="Sayaç : ${counter.value}")
+        
+        Button(onClick = {
+            counter.value += 1
+        }) {
+            Text(text = "Tıkla")
+        }
     }
 }
 
